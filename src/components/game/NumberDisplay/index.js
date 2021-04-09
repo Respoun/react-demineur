d432dcb8a3b52110ed9169f6efbfde8675533a2e
AntoineHaller <34535243+AntoineHaller@users.noreply.github.com>
2021-04-09 18:19:38 +0200

    Update index.js

--- src/components/game/NumberDisplay/index.js
+++ src/components/game/NumberDisplay/index.js
@@ -1,17 +1,27 @@
 import React from "react";
-
-import "./NumberDisplay.css";
+import styled from 'styled-components'
 
 const NumberDisplay = ({ value }) => {
   return (
-    <div className="NumberDisplay">
+    <NumberDisplayDiv>
       {value < 0
         ? `-${Math.abs(value)
             .toString()
             .padStart(2, "0")}`
         : value.toString().padStart(3, "0")}
-    </div>
+    </NumberDisplayDiv>
   );
 };
+
+const NumberDisplayDiv = styled.div`
+  width: 80px;
+  height: auto;
+  padding:8px;
+  border-radius:3px;
+  background: black;
+  font-size: 28px;
+  text-align: center;
+  color: white;
+`
 
 export default NumberDisplay;
